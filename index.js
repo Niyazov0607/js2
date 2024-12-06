85b2445679122af2b6c3c0bef7fcbd62d7e4d401
Niyazov0607 <shohjahonniyazov89@gmail.com>
2024-12-06 18:57:40 +0500

    js2

---
+++ index.js
@@ -0,0 +1,209 @@
+// if2
+
+// let number = +prompt("Butun son kiriting:");
+// if (number > 0) {
+//     number += 1 ;
+// }else {
+//     number -= 2;
+// }
+// console.log("natija" + number);
+
+// if3
+
+// let number = +prompt("Butun son kiriting:");
+// if (number > 0) {
+//     number += 1 ;
+// }else if(number < 0) {
+//     number -= 2;
+// }else {
+//     number += 10;
+// }
+// console.log("natija" + number);
+
+// if4
+
+// let numbe1 = +prompt("1-son kiriting");
+// let numbe2 = +prompt("2-son kiriting");
+// let numbe3 = +prompt("3-son kiriting");
+
+// let positiveCount = 0;
+
+// if (numbe1 > 0) {
+//     positiveCount++;
+// }
+// if (numbe2 > 0) {
+//     positiveCount++;
+// }
+// if (numbe3 > 0) {
+//     positiveCount++;
+// }
+// console.log(`Musbat sonlar soni: ${positiveCount}`);
+
+// if5
+
+// let numbe1 = +prompt("1-son kiriting");
+// let numbe2 = +prompt("2-son kiriting");
+// let numbe3 = +prompt("3-son kiriting");
+
+// let positiveCount = 0;
+// let negativeCount = 0;
+
+// if (numbe1 > 0) {
+//     positiveCount++;
+// }else if (numbe1 < 0){
+//     negativeCount++;
+// }
+// if (numbe2 > 0) {
+//     positiveCount++;
+// }else if (numbe1 < 0){
+//     negativeCount++;
+// }
+// if (numbe3 > 0) {
+//     positiveCount++;
+// }else if (numbe1 < 0){
+//     negativeCount++;
+// }
+// console.log(`Musbat sonlar soni: ${positiveCount} ,  Manfiy sonlar soni: ${negativeCount}`);
+
+// if6
+
+// let numbe1 = +prompt("katta son aniqlansin");
+// let numbe2 = +prompt("katta son aniqlansin");
+
+// let max = 0;
+
+// if (numbe1 > numbe2) {
+//     max = numbe1;
+// }else {
+//     max = numbe2;
+// }
+// console.log(`${max}`);
+
+// if7
+
+// let num1 = +prompt("1-sonni kiriting:");
+// let num2 = +prompt("2-sonni kiriting:");
+
+// if (num1 < num2) {
+//     console.log("Kichik son: 1-son");
+// } else if (num2 < num1) {
+//     console.log("Kichik son: 2-son");
+// } else {
+//     console.log("Ikkala son teng.");
+// }
+
+// if8
+
+// let num1 = +prompt("1-sonni kiriting:");
+// let num2 = +prompt("2-sonni kiriting:");
+
+// if (num1 > num2) {
+//     console.log(`Katta son: ${num1}`);
+//     console.log(`Kichik son: ${num2}`);
+// } else if (num2 > num1) {
+//     console.log(`Katta son: ${num1}`);
+//     console.log(`Kichik son: ${num2}`);
+// } else {
+//     console.log("Ikkala son teng.");
+// }
+
+// if9
+
+// let A = +prompt("A sonini kiriting:");
+// let B = +prompt("B sonini kiriting:");
+
+// if (A > B) {
+//     let temp = A; //temp = temporary degani - vaqtinchalik:
+//     A = B;
+//     B = temp;
+// }
+
+// console.log(`A = ${A}`);
+// console.log(`B = ${B}`);
+
+// if10
+
+// let A = +prompt("A sonini kiriting")
+// let B = +prompt("B sonini kiriting")
+
+// if (A != B) {
+//     console.log("Natija: " + (A + B));
+// }else {
+//     console.log("Natija: 0" );
+// }
+
+// if11
+
+// let A = +prompt("A sonini kiriting:");
+// let B = +prompt("B sonini kiriting:");
+
+// if (A !== B) {
+//     if (A > B) {
+//         console.log("Katta son: " + A);
+//     } else {
+//         console.log("Katta son: " + B);
+//     }
+// } else {
+//     console.log(0);
+// }
+
+//if12 
+
+// let numbe1 = +prompt("kichik son aniqlansin");
+// let numbe2 = +prompt("kichik son aniqlansin");
+
+// let min = 0;
+
+// if (numbe1 < numbe2) {
+//     min = numbe1;
+// }else {
+//     min = numbe2;
+// }
+// console.log(`${min}`);
+
+//if13 
+
+// let A = +prompt("1-sonni kiriting:");
+// let B = +prompt("2-sonni kiriting:");
+// let C = +prompt("3-sonni kiriting:");
+
+// if ((A > B && B > C) || (A < B && B < C)) {
+//     console.log("B", B);
+// }
+// if ((B > A && A > C) || (B < A && A < C)) {
+//     console.log("A", A);
+// }
+// if ((B > C && C > A) || (B < C && C < A)) {
+//     console.log("C", C);
+// }
+
+//if14 
+
+// let num1 = +prompt("1-sonni kiriting:");
+// let num2 = +prompt("2-sonni kiriting:");
+
+// if (num1 < num2) {
+//     console.log(`Kichik son: ${num2}`);
+//     console.log(`Katta son: ${num1}`);
+// } else if (num2 < num1) {
+//     console.log(`Kichik son: ${num2}`);
+//     console.log(`Katta son: ${num1}`);
+// } else {
+//     console.log("Ikkala son teng.");
+// }
+
+//if15 
+
+// let A = +prompt("A sonini kiriting:");
+// let B = +prompt("B sonini kiriting:");
+// let C = +prompt("C sonini kiriting:");
+
+// let sumAB = A + B;
+// let sumAC = A + C;
+// let sumBC = B + C;
+
+// let maxSum = Math.max(sumAB, sumAC, sumBC);
+
+// console.log("Eng katta yig'indi: " + maxSum);
+
+
